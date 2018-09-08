20c742b96e2d04d217dde7b28ed8786553499554
seanmullane <mullane.s.p@gmail.com>
2018-09-08 16:54:09 -0400

    added tests
    added tests to distance function

--- code/KNN.R
+++ code/KNN.R
@@ -1,7 +1,15 @@
 # KNN Implementation
 
+library(assertthat)
+
 dist.euclidean.vector <- function(x, y) {
   # Calculates the euclidean distance between two vectors
+  assertthat::assert_that(is.numeric(x))
+  assertthat::assert_that(is.numeric(y))
+  assertthat::assert_that(length(x)==length(y))
+  assertthat::assert_that(length(x)>0)
+  assertthat::assert_that(sum(is.na(x)) == 0)
+  assertthat::assert_that(sum(is.na(y)) == 0)
   return (sqrt(sum((x - y) ^ 2)))
 }
 
