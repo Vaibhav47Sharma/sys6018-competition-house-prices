955d0eaccde9b279fb33d49ab0c46942c925f90d
vaibhav47sharma <vs3br@virginia.edu>
2018-09-08 17:03:05 -0400

    fuction to calculate hamming distance

--- code/KNN.R
+++ code/KNN.R
@@ -13,5 +13,16 @@ dist.euclidean.vector <- function(x, y) {
   return (sqrt(sum((x - y) ^ 2)))
 }
 
+dist.hamming.vector <- function(x, y) {
+  # Calculates the hamming distance between two vectors
+  assertthat::assert_that(is.numeric(x))
+  assertthat::assert_that(is.numeric(y))
+  assertthat::assert_that(length(x)==length(y))
+  assertthat::assert_that(length(x)>0)
+  assertthat::assert_that(sum(is.na(x)) == 0)
+  assertthat::assert_that(sum(is.na(y)) == 0)
+  difference <- x - y
+  return (length(difference[difference != 0]))
+}
 
 
