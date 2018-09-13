8219befd57e90449aa53863c4244fc4c3b2e5e10
seanmullane <mullane.s.p@gmail.com>
2018-09-13 12:24:26 -0400

    Added cross validation function
    Added cross validation function

--- code/KNN.R
+++ code/KNN.R
@@ -1,6 +1,7 @@
 # KNN Implementation
 
 library(assertthat)
+library(tidyverse)
 
 Dist.euclidean.vector <- function(x, y) {
   # Calculates the euclidean distance between two vectors
@@ -119,3 +120,48 @@ KNN.Predict <- function(test, ycol, train, k, ...) {
   }
   return(predictions)
 }
+
+cross.val <- function(ycol, train, folds, ...) {
+  # This function takes a training data set and uses cross-validation to 
+  # measure the mean accuracy of a KNN model using given parameters.
+  assert_that(is.integer(folds))
+  assert_that(is.data.frame(train))
+  assert_that(is.numeric(ycol))
+  assert_that(folds > 1 & folds < nrow(train)/2)
+  assert_that(length(ycol) == nrow(train))
+  require(tidyverse)
+  #assert_that(is.integer(k))
+  
+  MSEs <- numeric(folds)
+  
+  # create folds
+  len <- nrow(train)
+  train.folds <- split(train, rep(1:folds, each=ceiling(len/folds), length.out=len))
+  y.folds <- split(ycol, rep(1:folds, each=ceiling(len/folds), length.out=len))
+    
+  for (i in 1:folds) {
+    # create test and train sets
+    test.subset <- train.folds[[i]]
+    train.inds <- (1:folds)[-i]
+    train.subset <- bind_rows(train.folds[train.inds])
+    ycol.test.subset <- y.folds[[i]]
+    ycol.train.subset <- unlist(y.folds[train.inds])
+    
+    # get predictions
+    preds <- KNN.Predict(test = as.matrix(test.subset), ycol = ycol.train.subset, train = as.matrix(train.subset), ...)
+    
+    # measure accuracy
+    MSE <- sum((preds - ycol.test.subset)^2)/len
+    MSEs[i] <- MSE
+  }
+  
+  outp <- c(mean(MSEs), sd(MSEs))
+  names(outp) <- c("Mean MSE", "SD of MSE")
+  
+  return(outp)
+}
+  
+  
+  
+  
+  
